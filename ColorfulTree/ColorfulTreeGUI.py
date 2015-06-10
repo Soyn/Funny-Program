8e7d023e7d7ee8f98a20368a5d15181e152f202c
Soyn <w604408482@163.com>
2015-06-10 08:59:58 +0800

    fix

--- ColorfulTree/ColorfulTreeGUI.py
+++ ColorfulTree/ColorfulTreeGUI.py
@@ -6,41 +6,47 @@ from Tkinter import *
 from random import *
 import turtle
 
-class DrawATree():
-    def __int__(self, BranchLength = 75, Step = 5):
+class DrawATree(object):
+    def __int__(self, BranchLength, Step):
         self.BranchLength = BranchLength
         self.Step = Step
 
-    def createGUI(self):
+
+    def GetText(self,str1, str2):
+        self.Step = str1.get()
+        self.BranchLength = str2.get()
+
+
+    def CreateGUI(self):
         TreeControl = Tk()
         TreeControl.title("Awesome-Tree")
         Label(TreeControl, text = "Step").grid(row = 0, column = 0)
-        e = StringVar()
-        Entry1 = Entry(TreeControl, textvariable  = e)
-        Entry1.grid(row = 0, column = 1)
-        self.Step = e.get()
-
         Label(TreeControl, text = "TreeLength").grid(row = 2, column = 0)
-        t = StringVar()
-        Entry2 = Entry(TreeControl, textvariable = t)
+        Entry1 = Entry(TreeControl)
+        Entry1.grid(row = 0, column = 1)
+        Entry2 = Entry(TreeControl)
         Entry2.grid(row = 2, column = 1)
-        self.BranchLength = t.get()
-
-
-        Button(TreeControl, text = "Draw A Tree", command = self.Draw).grid(row = 1, column = 2, rowspan = 3)
+        self.GetText(Entry1,Entry2)
+        Button(TreeControl, text = "Draw A Tree", command = self.show_Entry).grid(row = 1, column = 2, rowspan = 3)
         TreeControl.mainloop()
+
+
 
     def tree(self, t):
         t.pencolor((random(), random(), random()))
-        if self.BranchLength > 5:
-            print self.BranchLength
-            t.forward(self.BranchLength)
+        BL = self.BranchLength
+        ST = self.Step
+        if BL > 5:
+            t.forward(BL)
             t.right(10)
-            tree(self.BranchLength - self.Step, t)
+            tree(BL -ST, t)
             t.left(20)
-            tree(self.BranchLength - self.Step, t)
+            tree(BL - ST, t)
             t.right(10)
-            t.backward(self.BranchLength)
+            t.backward(BL)
+
+    def show_Entry(self):
+        print "BranchLength:%s\nStep:%s" %(self.BranchLength, self.Step)
 
     def Draw(self):
         t = turtle.Turtle()
@@ -57,4 +63,5 @@ class DrawATree():
 
 
 if __name__ == "__main__":
-    root = DrawATree().createGUI()
+    root = DrawATree()
+    root.CreateGUI()
