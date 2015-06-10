03cc4ebd289cc668f9425b0c36dd03f3936f60c4
Soyn <w604408482@163.com>
2015-06-10 23:43:15 +0800

    fix completed

--- ColorfulTree/ColorfulTreeGUI.py
+++ ColorfulTree/ColorfulTreeGUI.py
@@ -12,53 +12,54 @@ class DrawATree(object):
         self.Step = Step
 
 
-    def GetText(self,str1, str2):
-        self.Step = str1.get()
-        self.BranchLength = str2.get()
-
-
     def CreateGUI(self):
         TreeControl = Tk()
         TreeControl.title("Awesome-Tree")
-        Label(TreeControl, text = "Step").grid(row = 0, column = 0)
-        Label(TreeControl, text = "TreeLength").grid(row = 2, column = 0)
+        Label(TreeControl, text = "Step(5~20)").grid(row = 0, column = 0)
+        Label(TreeControl, text = "TreeLength(75~100)").grid(row = 2, column = 0)
+        Label(TreeControl, text = "PenSize(1~10)").grid(row = 3, column = 0)
         Entry1 = Entry(TreeControl)
         Entry1.grid(row = 0, column = 1)
         Entry2 = Entry(TreeControl)
         Entry2.grid(row = 2, column = 1)
-        self.GetText(Entry1,Entry2)
-        Button(TreeControl, text = "Draw A Tree", command = self.show_Entry).grid(row = 1, column = 2, rowspan = 3)
+        Entry3 = Entry(TreeControl)
+        Entry3.grid(row = 3, column = 1)
+        Button(TreeControl, text = "Quit", command = TreeControl.quit).grid(row = 2, column = 2)
+        Button(TreeControl, text = "Draw A Tree", command = lambda e1 = Entry1, e2 = Entry2, e3 = Entry3: self.Draw(e1, e2,e3)).grid(row = 3, column = 2, rowspan = 3)
         TreeControl.mainloop()
 
 
 
-    def tree(self, t):
+    def tree(self,bl, t):
         t.pencolor((random(), random(), random()))
-        BL = self.BranchLength
+        BL = bl
         ST = self.Step
         if BL > 5:
             t.forward(BL)
             t.right(10)
-            tree(BL -ST, t)
+            self.tree(BL -ST, t)
             t.left(20)
-            tree(BL - ST, t)
+            self.tree(BL - ST, t)
             t.right(10)
             t.backward(BL)
 
     def show_Entry(self):
         print "BranchLength:%s\nStep:%s" %(self.BranchLength, self.Step)
 
-    def Draw(self):
+    def Draw(self, e1, e2,e3):
+        self.Step = eval(e1.get())
+        self.BranchLength = eval(e2.get())
+        pensize = eval(e3.get())
         t = turtle.Turtle()
         t.hideturtle()
-        t.pensize(10)
+        t.pensize(pensize)
         t.speed(0)
         myWin = turtle.Screen()
         t.left(90)
         t.up()
         t.backward(100)
         t.down()
-        self.tree(t)
+        self.tree(self.BranchLength,t)
         myWin.exitonclick()
 
 
