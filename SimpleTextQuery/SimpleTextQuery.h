4dae42b45f5094fe69bad6e0a198bbca3c725679
Soyn <w604408482@163.com>
2015-07-15 17:38:50 +0800

    fix some bugs

--- SimpleTextQuery/SimpleTextQuery.h
+++ SimpleTextQuery/SimpleTextQuery.h
@@ -1,45 +1,44 @@
 #ifndef SIMPLETEXTQUERY_H_INCLUDED
 #define SIMPLETEXTQUERY_H_INCLUDED
 
-#include<string>
-#include<iostream>
-#include<vector>
-#include<set>
-#include<map>
-#include<fstream>
 #include<memory>
-
-using namespace std;
+#include <string>
+#include<vector>
+#include<map>
+#include<set>
+#include<algorithm>
+#include <fstream>
+#include <sstream>
+#include<iostream>
 /*
-*Simple TextQuery
+*@Breif:the head file of Ex12.30
 *@Soyn
-*Date:2015.7.14
 */
+using namespace std;
 
 class QueryResult;
+
 class TextQuery{
-/*This class is to query text*/
 public:
-    using LineNo = vector<string> :: size_type;
+    using line_no = vector<string> :: size_type;
     TextQuery(ifstream&);
-    QueryResult query( const string& ) const;
+    QueryResult query(const string&) const;
 private:
-    shared_ptr< vector<string> > input;                       //input is a smart point which refers to a vector, and the element in vector refers to the line of the text
-    map<string, shared_ptr< set<LineNo >>> result; //we can check the word in the map to see the line number
+    shared_ptr<vector<string> > file; //input file
+    //creat the map from word to line number
+    map<string, shared_ptr< set< line_no>>> wm;
 };
 
 class QueryResult{
-    /*this class is to show the result*/
+friend ostream& print(ostream&, const QueryResult);
 public:
-    friend ostream& print( ostream&, const QueryResult&);
-    QueryResult(const string& s, shared_ptr<set<TextQuery::LineNo>> Set, shared_ptr< vector<string>> v) : word(s),input(v){}
-private:
-    string word;
-    shared_ptr< set<TextQuery::LineNo>> nos;
-    shared_ptr< vector<string>> input;
+using line_no = vector<string> :: size_type;
+    QueryResult(string s, shared_ptr< set< line_no>> p, shared_ptr< vector<string>> f) : sought(s), lines(p), file(f){}
+
+public:
+    string sought ;// the word we want to query
+    shared_ptr<set< line_no>> lines; //the line number the word occurs
+    shared_ptr< vector< string> > file; //input file
 };
-
-ostream& print(ostream& , const QueryResult);
-
 
 #endif // SIMPLETEXTQUERY_H_INCLUDED
--- SimpleTextQuery/main.cpp
+++ SimpleTextQuery/main.cpp
@@ -1,41 +1,59 @@
-#include<sstream>
-#include<algorithm>
-#include "Ex12.27.h"
+#include<iostream>
+#include "SimpleTextQuery.h"
 
-/*
-* the Simple text query
-* Date:2015.7.14
-*/
 using namespace std;
 
-TextQuery :: TextQuery(ifstream &ifs) : input(new vector<string>){
-    LineNo lineNo(0);
-    for( string line, getline(ifs, line); ++lineNo){
-        input -> push_back(line);
-        istringstream line_stream(line);
-        for( string text, word; line_stream >> text; word.clear()){
-            //remove the punctation of the word(such as:  word,   )
-            remove_copy_if(text.begin(), text.end(), back_inserter(word), ispunct());
 
-            //read the word and the line number
-            auto &nos = result[word];
-
-            if(!nos) nos.reset(new set<lineNo>);  // if the word doesn't exsit
-            nos -> insert(LineNo);
+TextQuery :: TextQuery(ifstream& is) : file(new vector< string>)
+{
+    for(string text,word; getline(is, text);word.clear()){
+        file ->push_back(text);
+        int n = file ->size() - 1;
+        istringstream line(text);
+        while(line >> word){      //read each word in every line
+            auto &lines =   wm[word]; //lines refer to shared_ptr<set<line_no>>
+            if(!lines)   //if lines is null allocate a new set
+                lines.reset(new set<line_no>);
+            lines -> insert(n); //insert the line number
         }
     }
 }
-
-QueryResult TextQuery :: query(const string& str) const {
-    static shared_ptr<set<LineNo> > nodate(new set<LineNo>);
-    auto found = result.find(str);
-    if( found == result.end() ) return QueryResult( str, nodate, input);
-    else return QueryResult( str, found -> second, input);
+QueryResult  TextQuery :: query(const string &sought) const{
+    //if sought is not found,a point refers to this set will return
+    static shared_ptr<set< line_no>> nodata( new set<line_no>);
+    auto loc = wm.find(sought);
+    if (loc == wm.end())
+        return QueryResult(sought, nodata, file); // not found
+    else
+        return QueryResult(sought, loc -> second, file);
 }
 
-ostream print(ostream &out, const QueryResult &qr){
-    out << qr.word << "  occurs  " << qr.nos -> size() << (qr.size() > 1 ? " times" : " time") << endl;
-    for( auto i : *qr.nos)
-        out << "\t(line )" << i + 1 << ")   " << qr.input ->at(i) << endl;
-    return out;
+string make_plural(size_t ctr, const string &word, const string &ending)
+{
+    return (ctr > 1) ? word + ending : word;
+}
+ostream& Print(ostream& os, const QueryResult &qr)
+{
+    os << qr.sought << " occurs " << qr.lines -> size() << "  " << make_plural(qr.lines -> size(), "time", "s") << endl;
+    //print the line the sought occurs
+    for( auto num: *qr.lines)
+        os << "\t(line  " << num + 1 << ")" << *(qr.file ->begin() + num) << endl;
+    return os;
+}
+
+void runQueries( ifstream &infile)
+{
+    TextQuery tq(infile); //save file and create query-map
+    do{
+        cout << "enter word to look for, or q to quit: ";
+        string s;
+        if( !(cin >> s) || s == "q") break;
+        Print(cout, tq.query(s)) << endl;
+    }while(true);
+}
+int main()
+{
+    ifstream infile("test.txt");
+    runQueries(infile);
+    return 0;
 }
